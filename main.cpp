b946bc4f54b1d8dbdb66f46f42bd1c9f9c5badcf
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2025-01-15 16:36:29 +0200

    ex 3 is done

--- main.cpp
+++ main.cpp
@@ -2,43 +2,96 @@
 
 using namespace std;
 
-void insertionSort(int* arr, int size)
-{
-    for (int i = 1; i < size; ++i)
-    {
-        int key = arr[i];
-        int j = i - 1;
-
-        while (j >= 0 && arr[j] > key)
-        {
-            arr[j + 1] = arr[j];
-            --j;
-        }
-        arr[j + 1] = key;
-    }
-}
-
-void displayArray(const int* arr, int size)
+void displayGrades(const int* grades, int size)
 {
     for (int i = 0; i < size; ++i)
     {
-        cout << arr[i] << " ";
+        cout << "Grade " << i + 1 << ": " << grades[i] << endl;
     }
-    cout << endl;
+}
+
+void retakeExam(int* grades, int size)
+{
+    int index, newGrade;
+    cout << "Enter the index of the grade to change (1-10): ";
+    cin >> index;
+    if (index < 1 || index > size)
+    {
+        cout << "Invalid index.\n";
+        return;
+    }
+
+    cout << "Enter the new grade: ";
+    cin >> newGrade;
+
+    grades[index - 1] = newGrade;
+    cout << "Grade updated.\n";
+}
+
+void checkScholarship(const int* grades, int size)
+{
+    double sum = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        sum += grades[i];
+    }
+
+    double average = sum / size;
+    if (average >= 10.7)
+    {
+        cout << "Scholarship approved. Average grade: " << average << endl;
+    }
+
+    else
+    {
+        cout << "Scholarship denied. Average grade: " << average << endl;
+    }
+}
+
+void menu(int* grades, int size)
+{
+    int choice;
+    do {
+        cout << "\nMenu:\n";
+        cout << "1: Display grades\n";
+        cout << "2: Retake exam\n";
+        cout << "3: Check scholarship\n";
+        cout << "4: Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                displayGrades(grades, size);
+                break;
+            case 2:
+                retakeExam(grades, size);
+                break;
+            case 3:
+                checkScholarship(grades, size);
+                break;
+            case 4:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice.\n";
+        }
+    } while (choice != 4);
 }
 
 int main()
 {
     const int size = 10;
-    int arr[size] = {5, 1, 4, 2, 8, 3, 7, 6, 9, 0};
+    int grades[size];
 
-    cout << "Original array:\n";
-    displayArray(arr, size);
+    cout << "Enter 10 grades:\n";
+    for (int i = 0; i < size; ++i)
+    {
+        cin >> grades[i];
+    }
 
-    insertionSort(arr, size);
-
-    cout << "Sorted array:\n";
-    displayArray(arr, size);
+    menu(grades, size);
 
     return 0;
 }

0356bc721a3fec781c67de4765c50636c29e9e04
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2025-01-15 16:27:19 +0200

    ex 2 is done

--- main.cpp
+++ main.cpp
@@ -1,78 +1,39 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-void sortMobileNumbers(string* mobile, string* home, int size)
-{
-    for (int i = 0; i < size - 1; ++i)
-    {
-        for (int j = 0; j < size - i - 1; ++j)
-        {
-            if (mobile[j] > mobile[j + 1])
-            {
-                swap(mobile[j], mobile[j + 1]);
-                swap(home[j], home[j + 1]);
+void bubbleSortImproved(int* arr, int size) {
+    bool swapped;
+    for (int i = 0; i < size - 1; ++i) {
+        swapped = false;
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
+        if (!swapped) break;
     }
 }
 
-void sortHomeNumbers(string* mobile, string* home, int size)
-{
-    for (int i = 0; i < size - 1; ++i)
-    {
-        for (int j = 0; j < size - i - 1; ++j)
-        {
-            if (home[j] > home[j + 1])
-            {
-                swap(mobile[j], mobile[j + 1]);
-                swap(home[j], home[j + 1]);
-            }
-        }
+void displayArray(const int* arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
     }
+    cout << endl;
 }
 
-void displayUsers(const string* mobile, const string* home, int size)
-{
-    cout << "User list:\n";
-    for (int i = 0; i < size; ++i)
-    {
-        cout << "Mobile: " << mobile[i] << ", Home: " << home[i] << '\n';
-    }
-}
+int main() {
+    const int size = 10;
+    int arr[size] = {34, 12, 25, 78, 56, 7, 89, 45, 10, 19};
 
-int main()
-{
-    const int size = 5;
-    string mobile[size] = {"123456789", "987654321", "234567890", "456789012", "345678901"};
-    string home[size] = {"112233445", "554433221", "667788990", "998877665", "123443211"};
+    cout << "Original array:\n";
+    displayArray(arr, size);
 
-    int choice;
-    do {
-        cout << "\nMenu:\n1. Sort by mobile numbers\n2. Sort by home numbers\n3. Display users\n4. Exit\nEnter your choice: ";
-        cin >> choice;
+    bubbleSortImproved(arr, size);
 
-        switch (choice)
-        {
-            case 1:
-                sortMobileNumbers(mobile, home, size);
-                cout << "Sorted by mobile numbers.\n";
-                break;
-            case 2:
-                sortHomeNumbers(mobile, home, size);
-                cout << "Sorted by home numbers.\n";
-                break;
-            case 3:
-                displayUsers(mobile, home, size);
-                break;
-            case 4:
-                cout << "Exiting...\n";
-                break;
-            default:
-                cout << "Invalid choice. Try again.\n";
-        }
-    } while (choice != 4);
+    cout << "Sorted array:\n";
+    displayArray(arr, size);
 
     return 0;
 }
